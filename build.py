15796e83fb4b0695ea41bdea1054184bee74e329
Uilian Ries <uilianries@gmail.com>
2019-02-21 12:30:29 -0300

    Fix build script
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from cpt.packager import ConanMultiPackager
+import os
+import platform
 
+def get_os():
+    return platform.system().replace("Darwin", "Macos")
 
 if __name__ == "__main__":
+
+    arch = os.environ["ARCH"]
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add({"os" : get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
     builder.run()
